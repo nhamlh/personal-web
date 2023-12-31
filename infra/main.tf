aa8e424191b144a9b623938fa578c599889bee8d
Nham Le <nhamlh@Nhams-MacBook-Pro.local>
2023-12-31 22:48:06 +0700

    parameterize some infra attributes

--- infra/main.tf
+++ infra/main.tf
@@ -6,24 +6,12 @@ provider "gandi" {
 module "dns" {
   source = "./modules/dns"
 
-  server_public_ip = var.server_public_ip
-  base_domain      = var.base_domain
-  sub_domain       = var.sub_domain
-  dkim_policies = [
-    { selector = "stalwart", value = "\"v=DKIM1;k=rsa;p=${var.dkim_public_key}\"" },
-    { selector = "mailjet", value = "\"k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC35cALosfK5yNaeuH1fwSEey4d2GEqc9b0DaUW7BbfB1Unsces788FvieUZ3hyrOObSXyn9AhTRhLJmq9Jy+jR3pFlKVMPwv0G62Tzh6YvuE6Qsih7Ck0LUyowcDzRwH0+QMJJS/UO6cXe7WGZY9v1FEVJqmHCvHVtt5JYny01vwIDAQAB\"" },
-  ]
-
-  spf_domains = ["spf.mailjet.com"]
-
-  additional_records = [
-    # Mailjet domain validation
-    {
-      name   = "mailjet._68546c1f"
-      type   = "TXT"
-      values = ["68546c1f102bf5ed1ab21541c8c3e387"]
-    }
-  ]
+  server_public_ip   = aws.outputs.public_ip
+  base_domain        = var.base_domain
+  sub_domain         = var.sub_domain
+  dkim_policies      = var.dkim_policies
+  spf_domains        = var.spf_domains
+  additional_records = var.additional_records
 }
 
 module "aws" {
--- infra/vars.tf
+++ infra/vars.tf
@@ -19,3 +19,24 @@ variable "dkim_public_key" {
 variable "server_public_ip" {
   type = string
 }
+
+variable "dkim_policies" {
+  description = "Setup DKIM policies for secure email delivery"
+  type = list(object({
+    selector = string
+    value    = string
+  }))
+}
+
+variable "spf_domains" {
+  description = "Setup SPF policies for secure email delivery"
+  type        = list(string)
+}
+
+variable "additional_records" {
+  type = list(object({
+    type   = string
+    name   = string
+    values = list(string)
+  }))
+}
